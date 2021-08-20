261e93ff7c0544615284c7169b3b5084fe3f6a5c
yvsKaan <yavas.kaann@gmail.com>
2021-08-20 13:25:28 +0300

    fixed

--- users/settings.py
+++ users/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'customer',
 ]
 
