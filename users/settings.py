055f3ad6d499e5563d0d08e84991653e232b207c
yvsKaan <yavas.kaann@gmail.com>
2021-08-20 11:30:03 +0300

    heroku

--- users/settings.py
+++ users/settings.py
@@ -1,4 +1,5 @@
 import environ
+import os
 from pathlib import Path
 
 env = environ.Env()
@@ -108,3 +109,5 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.v2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
